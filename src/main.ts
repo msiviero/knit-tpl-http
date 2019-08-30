6ca28b97d597e3ce9c6f38a5773115a2eba8a2c8
Marco Siviero <m.siviero83@gmail.com>
2019-08-31 00:52:40 +0200

    Added clustering capabilities

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
+import * as cluster from "cluster";
+import { cpus } from "os";
 import { runner } from "./app";
 
-runner();
+const clusteringEnabled = (process.env.CLUSTERING || "false").toLowerCase() === "true";
+
+const execute = () => runner();
+const executeClustering = () => cluster.isMaster ? cpus().forEach(cluster.fork) : runner();
+
+if (clusteringEnabled) {
+  executeClustering();
+} else {
+  execute();
+}
